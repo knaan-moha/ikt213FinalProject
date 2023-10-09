b1207f3960e3404dafbd33fbcd2f3dcf0ac8a004
Katsiaryna Labkouskaya <katsiarynl@uia.no>
2023-10-09 15:00:41 +0200

    :sparkles:feat: add main and brightness features

--- main.py
+++ main.py
@@ -1,6 +1,45 @@
-print("Helloo Katee ")
+import cv2
+import time
+from cvzone.HandTrackingModule import HandDetector
+import selfie as s
+import volume as v
+import brightness as b
 
-import mediapipe as mp 
+detector = HandDetector(detectionCon=0.9, maxHands=2)
+  
 
-mp_hands = mp.solutions.hands
-hands = mp_hands.Hands()
+cap = cv2.VideoCapture(0)
+
+def main(cap, detector): 
+    while True: 
+        ret, img = cap.read()
+        hands, img = detector.findHands(img, draw=True, flipType=True)
+        k= cv2.waitKey(125)
+    
+       
+        if hands:
+            hand1 = hands[0]
+            finger_up = detector.fingersUp(hand1)  
+            lmList1=hand1["lmList"]
+            if hands[0]["type"]=="Left":  
+                if finger_up==[0,1,1, 0, 0]:
+                  s.test_activate_selfie(cap, detector)
+                  print("selfie")
+                if finger_up.count(1)!=2: 
+                  v.test2_control_volume(img, finger_up) 
+                if finger_up==[1, 1, 0, 0, 0]: 
+                   b.test_control_brightness(img, detector, lmList1)
+               
+                # v.test2_control_volume(img, finger_up) 
+            
+          
+        
+            if hands[0]["type"]=="Right": 
+               pass
+        cv2.imshow('PC_Control_System', img)
+        
+        if k == 27:
+            break
+       
+
+main(cap, detector)
