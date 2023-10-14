80c9b0c64dd44dfa7d09d9abdee04ac5ef4f3279
Katsiaryna Labkouskaya <katsiarynl@uia.no>
2023-10-14 20:36:30 +0200

    feat: add virtual mouse f

---
+++ activate_mouse.py
@@ -0,0 +1,42 @@
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+import HandTrackingModule as htm
+import numpy as np 
+import  pyautogui
+import time
+import HandTrackingModule as htm
+
+
+
+def activate_mouse(img, hand_img, detector, fingers_up, frame_reduction, web_cam_width, web_cam_height, screen_width, screen_height, smoothing, prev_loc_x, prev_loc_y): 
+    x_b, y_b =prev_loc_x, prev_loc_y
+
+    Land_mark_list, bounding_box = detector.findPosition(hand_img)
+    if len(Land_mark_list) != 0: 
+        finger_index_tip_x, finger_index_tip_y = Land_mark_list[8][1:]; 
+        middle_finger_tip_x,  middle_finger_tip_y = Land_mark_list[12][1:];
+    
+        #print(open_fingers)
+    if fingers_up.count(1) == 1:
+        pass
+        # # drawing  a rectangle 
+        cv2.rectangle(img, (frame_reduction, frame_reduction), (web_cam_width-frame_reduction, web_cam_height- frame_reduction), (0, 128, 0), 3)
+
+        # # Convert the coordinates
+        x3 = np.interp(finger_index_tip_x, (frame_reduction, web_cam_width-frame_reduction), (0, screen_width))
+        y3 = np.interp(finger_index_tip_y, (frame_reduction, web_cam_height-frame_reduction), (0, screen_height))
+
+        # # # smoothing mouse movements 
+        current_x = prev_loc_x +(x3 -prev_loc_x)/smoothing
+        current_y = prev_loc_y +(y3 -prev_loc_y)/smoothing
+
+        # # Move the mouse cursor to the new coordinates
+        pyautogui.moveTo(current_x, current_y)
+        cv2.circle(img, (finger_index_tip_x, finger_index_tip_y ), 15, (128,0,128), cv2.FILLED)
+        prev_loc_x, prev_loc_y = current_x, current_y
+        cv2.putText(img,"Mouse", (20, 200), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+        if prev_loc_x>=0 and prev_loc_y>=0: 
+           return prev_loc_x, prev_loc_y
+        else:
+           return x_b, y_b
+        
--- volume.py
+++ volume.py
@@ -2,29 +2,28 @@ import pyautogui
 import cv2
 import time
 from cvzone.HandTrackingModule import HandDetector
+import functions as f 
 
 
-
-def test2_control_volume(img, finger_up):
+def control_volume(img, finger_up):
           
         # https://www.geeksforgeeks.org/set-countdown-timer-to-capture-image-using-python-opencv/
-            if finger_up.count(1) == 3:
-                
-                cv2.putText(img,"Descreasing volume", (20, 460), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
-              
+            if finger_up== [0, 0, 1 ,1, 1]:
+                f.print_action(img, "Descreasing volume")  
                 pyautogui.press('volumedown', presses=1)
                 
        
                 
-            if finger_up.count(1) == 4:
+            if finger_up== [0, 1, 1, 1, 1]:
+                f.print_action(img, "Increasing volume")  
                 pyautogui.press('volumeup', presses=1)
-               
-                cv2.putText(img,"Increasing volume", (20, 460), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
+                
 
             if finger_up==[0, 1, 0, 0, 1]: 
-                cv2.putText(img, "Mute", (20, 460), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
+                f.print_action(img, "Mute/Unmute Volume")  
+
                 pyautogui.press('volumemute', presses=1, interval=0.35)
-                print("mute")
+               
            
         # check for the key pressed
         
