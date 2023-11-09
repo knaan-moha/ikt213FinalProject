8a620564cb7bcaac1f11d5db9fb0739eaf165d3a
Katsiaryna Labkouskaya <katsiarynl@uia.no>
2023-11-09 22:43:38 +0100

    :recycle:refactor: fix params for scrolling

--- utilities/scrolling.py
+++ utilities/scrolling.py
@@ -1,18 +1,14 @@
 import pyautogui
 import functions as f
 
-def perform_scrolling(img, fingers_up,  thumb_tips_right, thumb_base_right, thumb_tips_left, thumb_base_left ): 
+def perform_scrolling(img, lmList, fingers_up ): 
     """
      Performs scrolling up and down
      :param img: Frame for displaying the invoked command (i.e. scolling up or down).
      :param lmList: List of detected hand landmarks.
      :param fingers_up: Number of raised fingers.
      """
-    
-    ##get_thumb_direction_pointing_right  = lmList[4][0] > lmList[17][0]
-    ##get_thumb_direction_pointing_left = lmList[4][0] < lmList[17][0]
-    
-  
+
 
 
     if sum(fingers_up[0:2]) == 2 and sum(fingers_up[2:]) == 0:                 
@@ -24,14 +20,16 @@ def perform_scrolling(img, fingers_up,  thumb_tips_right, thumb_base_right, thum
                                 f.print_action(img, "Scrolling Down")
                                 pyautogui.scroll(-3)
     elif (fingers_up[0] == 1) and sum(fingers_up[1:]) == 0:
-         # for the left direction
-        if thumb_tips_right[0] < thumb_base_right[0]:
+    
+
+         
+        if lmList[5][1] < lmList[6][1]:
             f.print_action(img, "Scrolling left")
-            pyautogui.hscroll(60);
+            pyautogui.hscroll(20);
             
         # for the right direction
-        elif thumb_tips_left[0] < thumb_base_left[0]:
+        elif lmList[5][1] > lmList[6][1]:
             f.print_action(img, "Scrolling right")
-            pyautogui.hscroll(-60);
+            pyautogui.hscroll(-20);
 
 
