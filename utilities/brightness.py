def56b6ddf9a3b00b784b2a13208369b21edb357
Katsiaryna Labkouskaya <katsiarynl@uia.no>
2023-11-12 23:19:57 +0100

    :recycle: refactor: remove redunt if statements for mac

--- utilities/brightness.py
+++ utilities/brightness.py
@@ -19,74 +19,52 @@ def control_brightness(img, detector, lmList, fingers_up):
     if fingers_up==[1, 1, 0, 0, 0]:    
         length, _, img = detector.findDistance(lmList[4][0:2], lmList[8][0:2], img, color=(255, 0, 255),
                                                     scale=10)    
-        # if platform.system()== "Windows":      
-        #     if (length>360): 
-        #         f.print_action(img, "Brightness: 100 ")
-        #         sbc.set_brightness(100, display=0)
-        #     elif (length<=360 and length>320): 
-        #         f.print_action(img, "Brightness: 90 ")
-        #         sbc.set_brightness(90, display=0)
-        #     elif (length<=320 and length>280): 
-        #         f.print_action(img, "Brightness: 80 ")
-        #         sbc.set_brightness(80, display=0)
-        #     elif (length<=280 and length>240): 
-        #         f.print_action(img, "Brightness: 70 ")
-        #         sbc.set_brightness(70, display=0)
-        #     elif (length<=240 and length>200): 
-        #         f.print_action(img, "Brightness: 60 ")
-        #         sbc.set_brightness(60, display=0)
-        #     elif (length<=200 and length>160): 
-        #         f.print_action(img, "Brightness: 50 ")
-        #         sbc.set_brightness(50, display=0)
-        #     elif (length<=160 and length>120): 
-        #         f.print_action(img, "Brightness: 40 ")
-        #         sbc.set_brightness(40, display=0)
-        #     elif (length<=120 and length>80): 
-        #         f.print_action(img, "Brightness: 30 ")
-        #         sbc.set_brightness(30, display=0)
-        #     elif (length<=80 and length>40): 
-        #         f.print_action(img, "Brightness: 20 ")
-        #         sbc.set_brightness(20, display=0)
-        #     elif (length<=40 and length>0): 
-        #         f.print_action(img, "Brightness: 10 ")
-        #         sbc.set_brightness(10, display=0)
-        #     else: 
-        #         cv2.putText(img, str(length), (20, 460), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
-        
-            
-        if (length>360): 
+        if platform.system()== "Windows":      
+            if (length>360): 
                 f.print_action(img, "Brightness: 100 ")
-                subprocess.run(['osascript', '-e', 'tell application "System Events"', '-e', 'key code 113', '-e', 'end tell'])
-        elif (length<=360 and length>320): 
+                sbc.set_brightness(100, display=0)
+            elif (length<=360 and length>320): 
                 f.print_action(img, "Brightness: 90 ")
-                subprocess.run(['osascript', '-e', 'tell application "System Events"', '-e', 'key code 145', '-e', 'end tell'])
-        elif (length<=320 and length>280): 
+                sbc.set_brightness(90, display=0)
+            elif (length<=320 and length>280): 
                 f.print_action(img, "Brightness: 80 ")
-                subprocess.run(['osascript', '-e', 'tell application "System Events"', '-e', 'key code 145', '-e', 'end tell'])
-        elif (length<=280 and length>240): 
+                sbc.set_brightness(80, display=0)
+            elif (length<=280 and length>240): 
                 f.print_action(img, "Brightness: 70 ")
-                subprocess.run(['osascript', '-e', 'tell application "System Events"', '-e', 'key code 145', '-e', 'end tell'])
-        elif (length<=240 and length>200): 
+                sbc.set_brightness(70, display=0)
+            elif (length<=240 and length>200): 
                 f.print_action(img, "Brightness: 60 ")
-                subprocess.run(['osascript', '-e', 'tell application "System Events"', '-e', 'key code 145', '-e', 'end tell'])
-        elif (length<=200 and length>160): 
+                sbc.set_brightness(60, display=0)
+            elif (length<=200 and length>160): 
                 f.print_action(img, "Brightness: 50 ")
-                subprocess.run(['osascript', '-e', 'tell application "System Events"', '-e', 'key code 145', '-e', 'end tell'])
-        elif (length<=160 and length>120): 
+                sbc.set_brightness(50, display=0)
+            elif (length<=160 and length>120): 
                 f.print_action(img, "Brightness: 40 ")
-                subprocess.run(['osascript', '-e', 'tell application "System Events"', '-e', 'key code 145', '-e', 'end tell'])
-        elif (length<=120 and length>80): 
+                sbc.set_brightness(40, display=0)
+            elif (length<=120 and length>80): 
                 f.print_action(img, "Brightness: 30 ")
-                subprocess.run(['osascript', '-e', 'tell application "System Events"', '-e', 'key code 145', '-e', 'end tell'])
-        elif (length<=80 and length>40): 
+                sbc.set_brightness(30, display=0)
+            elif (length<=80 and length>40): 
                 f.print_action(img, "Brightness: 20 ")
-                subprocess.run(['osascript', '-e', 'tell application "System Events"', '-e', 'key code 145', '-e', 'end tell'])
-        elif (length<=40 and length>0): 
+                sbc.set_brightness(20, display=0)
+            elif (length<=40 and length>0): 
                 f.print_action(img, "Brightness: 10 ")
-                subprocess.run(['osascript', '-e', 'tell application "System Events"', '-e', 'key code 145', '-e', 'end tell'])
-        else: 
+                sbc.set_brightness(10, display=0)
+            else: 
                 cv2.putText(img, str(length), (20, 460), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
+        
+        elif platform.system()=="Darwin":
+                
+                if length>360 and length<50: 
+                        f.print_action(img, "Brightness: 100 ")
+                        subprocess.run(['osascript', '-e', 'tell application "System Events"', '-e', 'key code 113', '-e', 'end tell'])
             
-             
-     
+                elif length>=50 and length<0: 
+                        f.print_action(img, "Brightness: 10 ")
+                        subprocess.run(['osascript', '-e', 'tell application "System Events"', '-e', 'key code 145', '-e', 'end tell'])
+                else: 
+                        cv2.putText(img, str(length), (20, 460), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
+                
+                
+        
                        
