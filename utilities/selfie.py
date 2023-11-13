9f4a91c5d81ab6e9e0abbf30a99813cd6632f697
Katsiaryna Labkouskaya <katsiarynl@uia.no>
2023-11-13 20:40:01 +0100

    :memo:refactor: add comments for selfie

--- utilities/selfie.py
+++ utilities/selfie.py
@@ -12,25 +12,30 @@ def activate_selfie(img, fingers_up):
     :param img: Frame for notifying when the command can be invoked (i.e. countdown for selfie)
     :param fingers_up: Number of raised fingers.
     """
-   
+    # check if the raised fingers ar the index finger and the middle finger 
     if fingers_up==[0,1,1, 0, 0]:
         
-
+        # check if the count up timer has reached 3 seconds (or more)
         if time.time()-m.timer_selfie>=3:
           
-           
+           # save the input frame in the repository 
            cv2.imwrite('taken_selfie.jpg', img)
        
        
            cv2.waitKey(1000)
+        # update the count up timer 
            m.timer_selfie=time.time()
+        #return the True value to display the bounding box of the hand 
            return True
            
         else:
             f.print_action(img, "Selfie in "+str(4-(time.time()-m.timer_selfie))[0])
+            #return the False value to hide the bounding box of the hand 
             return False 
     else: 
+        # update the count up timer 
         m.timer_selfie =time.time()
+        #return the True value to display the bounding box of the hand 
         return True        
         
 
