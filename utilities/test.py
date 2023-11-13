ea56669b0bd09c952ff816ab68aa15926fbd90b7
Katsiaryna Labkouskaya <katsiarynl@uia.no>
2023-11-13 22:20:54 +0100

    :fire:refactor: remove redunt files

--- utilities/test.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-def set_brightness():
-    subprocess.run(['osascript', '-e', 'tell application "System Events"', '-e', 'key code 113', '-e', 'end tell'])
-    
-    #subprocess.run(['osascript', '-e', 'tell application "System Events"', '-e', 'key code 145', '-e', 'end tell'])
-
-# Test the function with a brightness value
-set_brightness()  # Adjust the brightness value as needed
