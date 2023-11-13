b7bcb6cc07351513785db641074ffcb6e439f2fa
Katsiaryna Labkouskaya <katsiarynl@uia.no>
2023-11-13 19:08:16 +0100

    :refactor: fix: add condition in keystroke to prevent redunt activation

--- utilities/space_keystroke.py
+++ utilities/space_keystroke.py
@@ -3,15 +3,15 @@ import pyautogui
 import functions as f
 
 
-def space_keystroke(img, fingers_up):
+def space_keystroke(img, lmList, fingers_up):
     """
      Triggers space keystroke.
      :param img: Frame for displaying the invoked command (i.e. space keystroke).
      :param fingers_up: Number of raised fingers.
     """
-
-
-    if fingers_up.count(1) == 5:
+    
+    print(fingers_up.count(1))
+    if fingers_up.count(1) == 5 and lmList[6][1]<lmList[0][1]:
 
         f.print_action(img, 'Space keystroke')
         pyautogui.press('space', presses=1)
